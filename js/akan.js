13009b62bdf3ff043d8f8c5cd4f1c37ad0e7d501
Vector254 <ochrist7@gmail.com>
2020-07-17 11:34:13 +0300

    add a function to clear the output data

--- js/akan.js
+++ js/akan.js
@@ -1,116 +1,118 @@
 function validate (){
     var date=new Date();
-    var today=date.getFullYear();
-    var year=parseInt(document.getElementById("year").value);
+    var today=date.getFullYear();//get the current year to use it later for validation
+    var year=parseInt(document.getElementById("year").value);//get relevant input and convert to interger
     var month=parseInt(document.getElementById("month").value);
     var day=parseInt(document.getElementById("day").value);
-    if(year="" || year>today ||year<1900)
+    if(year="" || year>today ||year<1900) //validate the year entered against current year and least possible year
         {
             alert("please enter a valid year of birth!");
             return false;
         }
-    if(month="" || month>12 ||month<1)
+     else if(month="" || month>12 ||month<1)//validate month entered is not less than 1 or more than 12
         {
             alert("please enter a valid month");
             return false;
         }
         
-    if (month=("4"||"6"||"9"||"11") && day>30)
+    else if (month=("4"||"6"||"9"||"11") && day>30)//validate input months with 30 days do not not exceed the 30 days
         {
             alert("This month has 30 days!");
             return false;
         }
-    if (month="2"&& day>28)
+    else if (month="2"&& day>28)//validate february does not exceed 28
         {
             alert("This month in that year had 28 days!")
             return false;
         }
-    if(day="" || day<1 || day>31)
+    else if(day="" || day<1 || day>31)
         {
             alert("please enter a valid day");
             return false;
         }
+    else {return true;}
+    
     }
 
 function generateName(){
-var CC=parseInt( document.getElementById("year").value.slice(0,2));
+var CC=parseInt( document.getElementById("year").value.slice(0,2));//declare variables and convert them to integers
 var YY=parseInt(document.getElementById("year").value.slice(2,4));
 var MM=parseInt(document.getElementById("month").value);
 var DD=parseInt(document.getElementById("day").value);
 var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-var Day=( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7;
-var exactDay=Day.toFixed();
+var Day=( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7; //calculate the day of the week 
+var exactDay=Day.toFixed();// convert the day calculated into a whole number
 var gender=parseInt(document.getElementById("gender").value);
 
-if (gender=="1"){
+if (gender=="1"){  //check gender against day of week and display the relevant akan name
     if (exactDay==="0")
         {
-            document.getElementById("result").value="You were born on sunday, your akan name is:"+maleNames[0];    
+            document.getElementById("result").value="Born on Sunday, your akan name is:"+maleNames[0];    
         }
     else if (exactDay==="1")
         {
-            document.getElementById("result").value="You were born on monday, your akan name is:"+maleNames[1];
+            document.getElementById("result").value="Born on Monday, your akan name is:"+maleNames[1];
         }
     else if (exactDay==="2")
         {
-            document.getElementById("result").value="You were born on tuesday, your akan name is:"+maleNames[2];
+            document.getElementById("result").value="Born on Tuesday, your akan name is:"+maleNames[2];
         }
     else if (exactDay==="3")
         {
-            document.getElementById("result").value="You were born on wednesday, your akan name is:"+maleNames[3];
+            document.getElementById("result").value="Born on Wednesday, your akan name is:"+maleNames[3];
         }
     else if (exactDay==="4")
         {
-            document.getElementById("result").value="You were born on thursday, your akan name is:"+maleNames[4];
+            document.getElementById("result").value="Born on thursday, your akan name is:"+maleNames[4];
         }
     else if (exactDay==="5")
         {
-            document.getElementById("result").value="You were born on friday, your akan name is:"+maleNames[5];
+            document.getElementById("result").value="Born on friday, your akan name is:"+maleNames[5];
         }
     else
         {
-            document.getElementById("result").value="You were born on saturday, your akan name is:"+maleNames[6];
+            document.getElementById("result").value="Born on saturday, your akan name is:"+maleNames[6];
         }
     }
 
 if (gender=="2"){
     if (exactDay==="0")
         {
-            document.getElementById("result").value="You were born on sunday, your akan name is:"+femaleNames[0];
+            document.getElementById("result").value="Born on Sunday, your akan name is:"+femaleNames[0];
         }
     else if (exactDay==="1")
         {
-            document.getElementById("result").value="You were born on monday, your akan name is:"+femaleNames[1];
+            document.getElementById("result").value="Born on Monday, your akan name is:"+femaleNames[1];
         }
     else if (exactDay==="2")
         {
-            document.getElementById("result").value="You were born on tuesday, your akan name is:"+femaleNames[2];
+            document.getElementById("result").value="Born on Tuesday, your akan name is:"+femaleNames[2];
         }
     else if (exactDay==="3")
         {
-            document.getElementById("result").value="You were born on wednesday, your akan name is:"+femaleNames[3];
+            document.getElementById("result").value="Born on Wednesday, your akan name is:"+femaleNames[3];
         }
     else if (exactDay==="4")
         {
-            document.getElementById("result").value="You were born on thursday, your akan name is:"+femaleNames[4];
+            document.getElementById("result").value="Born on Thursday, your akan name is:"+femaleNames[4];
         }
     else if (exactDay==="5")
         {
-            document.getElementById("result").value="You were born on friday, your akan name is:"+femaleNames[5];
+            document.getElementById("result").value="Born on Friday, your akan name is:"+femaleNames[5];
         }
     else
         {
-            document.getElementById("result").value="You were born on saturday, your akan name is:"+femaleNames[6];
+            document.getElementById("result").value="Born on Saturday, your akan name is:"+femaleNames[6];
         }
     }
 }
 function clearData()
     {
-        document.getElementById("form").reset();
+        document.getElementById("form").reset();//clear form input data
     }
 
-function clearOutput()
+function clearAll()
     {
-        document.getElementById("result").reset();
+        document.getElementById("output").reset();
     }
