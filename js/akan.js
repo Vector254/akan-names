13f9807a4fc3afe3147fa9d3898c414ba41b638b
Vector254 <ochrist7@gmail.com>
2020-07-20 09:56:13 +0300

    rectified the error where the day was greater the actual day by 1

--- js/akan.js
+++ js/akan.js
@@ -44,7 +44,7 @@ var MM=parseInt(document.getElementById("month").value);
 var DD=parseInt(document.getElementById("day").value);
 var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-var Day=( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7; //calculate the day of the week 
+var Day=(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7)-1; //calculate the day of the week 
 var exactDay=Day.toFixed();// convert the day calculated into a whole number
 var gender=parseInt(document.getElementById("gender").value);
 
